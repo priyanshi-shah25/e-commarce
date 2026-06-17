bce70cd36aee5fcfad6970af29a19545f4457559
priyanshi-shah25 <priyanshi.s@devstree.in>
2026-06-17 09:37:06 +0530

    Remove old project files

--- src/store/product.strore.ts
+++
@@ -1,9 +0,0 @@
-import { create } from "zustand";
-import { CartState } from "@/types/product";
-
-export const useCartStore = create<CartState>((set) => ({
-  cart: [],
-  addToCart: (product) => set((state) => ({ 
-    cart: [...state.cart, product] 
-  })),
-}));
